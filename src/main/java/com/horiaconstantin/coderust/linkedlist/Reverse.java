f91a88b30b7e6f15d904f18daadabf60b560ef6d
Horia Constantin <me@horiaconstantin.com>
2021-11-26 16:50:02 +0100

    removed reverse implementation

--- src/main/java/com/horiaconstantin/coderust/linkedlist/Reverse.java
+++ src/main/java/com/horiaconstantin/coderust/linkedlist/Reverse.java
@@ -3,18 +3,6 @@ package com.horiaconstantin.coderust.linkedlist;
 class Reverse {
 
 	public static LinkedListNode reverse(LinkedListNode head) {
-		if (head == null || head.next == null) {
-			return head;
-		}
-		LinkedListNode previous = null;
-		LinkedListNode current = head;
-		while (current.next != null) {
-			LinkedListNode next = current.next;
-			current.next = previous;
-			previous = current;
-			current = next;
-		}
-		current.next = previous;
-		return current;
+		return head;
 	}
 }
